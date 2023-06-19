277131891383430fc2098386410016507425c8f0
Matheus <matheus.andrade.lopes@hotmail.com>
2023-06-19 20:35:30 -0300

    Conexão com a nossa API

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import Input from '../Input';
 import styled from 'styled-components';
 import { useState } from 'react';
-import { livros } from './dadosPesquisa'
+import { useEffect } from 'react';
+import { getLivros } from '../../servicos/livros';
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -41,10 +42,19 @@ const Resultado = styled.div`
     }
 `
 
-
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
-    
+    const [ livros, setLivros ] = useState([]);
+
+    useEffect(() => {
+        fetchLivros();
+    }, [])
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros();
+        setLivros(livrosDaAPI)
+    }
+
     return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
