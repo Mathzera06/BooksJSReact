90487b81ccf4adeb68ec456e380c0eb19ee081c2
Matheus <matheus.andrade.lopes@hotmail.com>
2023-06-20 22:16:05 -0300

    Atualização da página

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { getLivros } from '../../servicos/livros';
+import { postFavorito } from '../../servicos/favoritos';
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -55,6 +56,11 @@ function Pesquisa() {
         setLivros(livrosDaAPI)
     }
 
+    async function insertFavorito(id) {
+        await postFavorito(id);
+        alert(`Livro de ${id} inserido!`)
+    }
+
     return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
@@ -68,7 +74,7 @@ function Pesquisa() {
                 }}
             />
             {livrosPesquisados.map(livro => (
-                <Resultado>
+                <Resultado onClick={() => insertFavorito(livro.id)}>
                     <p>{livro.nome}</p>
                     <img src={livro.src}/>
                 </Resultado>
--- src/rotas/favoritos.js
+++ src/rotas/favoritos.js
@@ -1,21 +1,83 @@
-import Pesquisa from '../componentes/Pesquisa';
-import Header from '../componentes/Header'
-import styled from 'styled-components';
- 
+import { useEffect } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components'
+import { deleteFavorito, getFavoritos } from '../servicos/favoritos';
+
+
 const AppContainer = styled.div`
-  {
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(90deg,#002F52 35%, #326589 165%);
-  }
+   width: 100vw;
+   height: 100vh;
+   background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
+
+const ResultadoContainer = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+`
+
+const Resultado = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 20px 0;
+   cursor: pointer;
+   text-align: center;
+   padding: 0 100px;
+   p {
+       width: 200px;
+       color: #FFF;
+   }
+   img {
+       width: 100px;
+   }
+   &:hover {
+       border: 1px solid white;
+   }
+`
+
+const Titulo = styled.h2`
+   color: #FFF;
+   font-size: 36px;
+   text-align: center;
+   width: 100%;
+   padding-top: 35px
 `
 
 function Favoritos() {
-  return (
-    <AppContainer>
-      <Pesquisa/>
-    </AppContainer>
-  );
+ const [favoritos, setFavoritos] = useState([]);
+
+ async function fetchFavoritos(){
+  const favoritosDaAPI = await getFavoritos()
+  setFavoritos(favoritosDaAPI)
+ }
+
+ async function deletarFavoritos(id) {
+  await deleteFavorito(id)
+  await fetchFavoritos();
+  alert(`Livro de id: ${id} deletado!`)
 }
 
-export default Favoritos;
+ useEffect(() => {
+  fetchFavoritos()
+ }, []);
+  return (
+   <AppContainer>
+     <div>
+       <Titulo>Aqui estão seus livros favoritos:</Titulo>
+       <ResultadoContainer>
+         {
+           favoritos.length !== 0 ? favoritos.map(favorito => (
+             <Resultado onClick={() => deletarFavoritos(favorito.id)}>
+               <p>{favorito.nome}</p>
+             </Resultado>
+           )) : null
+         }
+       </ResultadoContainer>
+     </div>
+   </AppContainer>
+ );
+}
+
+
+export default Favoritos
